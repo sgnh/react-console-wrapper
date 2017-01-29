6b93ddc8d3cb5f12490d58047e77deceec9aab7b
Steffen Hansen <sgnh@users.noreply.github.com>
2017-01-29 06:55:18 +0100

    Add missing functions from console api

--- src/Console.js
+++ src/Console.js
@@ -1,39 +1,111 @@
 import React from 'react';
 
-const { string, bool } = React.PropTypes;
+const { array, string, bool, shape } = React.PropTypes;
 
 const Console = (props) => {
-  if (props.log) {
-    console.log(props.log);
-  }
-
-  if (props.info) {
-    console.info(props.info);
-  }
-
-  if (props.error) {
-    console.error(props.error);
+  if (props.assert.assertion) {
+    console.assert(props.assert.assertion, props.assert.message);
   }
 
   if (props.clear) {
     console.clear();
   }
 
+  if (props.count) {
+    console.count(props.count);
+  }
+
+  if (props.error) {
+    console.error(props.error);
+  }
+
+  if (props.group) {
+    console.group();
+  }
+
+  if (props.groupColapsed) {
+    console.groupCollapsed();
+  }
+
+  if (props.groupEnd) {
+    console.groupEnd();
+  }
+
+  if (props.info) {
+    console.info(props.info);
+  }
+
+  if (props.log) {
+    console.log(props.log);
+  }
+
+  if (props.table.data.length) {
+    console.table(props.table.data, props.table.columns);
+  }
+
+  if (props.time) {
+    console.time(props.time);
+  }
+
+  if (props.timeEnd) {
+    console.timeEnd(props.timeEnd);
+  }
+
+  if (props.trace) {
+    console.trace();
+  }
+
+  if (props.warn) {
+    console.warn(props.warn);
+  }
+
   return null;
 };
 
 Console.propTypes = {
-  log: string,
-  info: string,
-  error: string,
+  assert: shape({
+    assertion: bool,
+    message: string,
+  }),
   clear: bool,
+  count: string,
+  error: string,
+  group: bool,
+  groupColapsed: bool,
+  groupEnd: bool,
+  info: string,
+  log: string,
+  table: shape({
+    data: array,
+    columns: array,
+  }),
+  time: string,
+  timeEnd: string,
+  trace: bool,
+  warn: string,
 };
 
 Console.defaultProps = {
-  log: '',
-  info: '',
-  error: '',
+  assert: {
+    assertion: false,
+    message: '',
+  },
   clear: false,
+  count: '',
+  error: '',
+  group: false,
+  groupColapsed: false,
+  groupEnd: false,
+  info: '',
+  log: '',
+  table: {
+    data: [],
+    columns: [],
+  },
+  time: '',
+  timeEnd: '',
+  trace: false,
+  warn: '',
 };
 
 export default Console;
